b5a500767b1f3e1ab8ec20c11e8e9024d3a54e54
Floris Bos <bos@je-eigen-domein.nl>
2019-07-17 17:22:06 +0200

    Fix setting /etc/nslcd.conf when using external LDAP/AD server
    When an external server is used the piserver.pem TLS
    certificate is not being generated.
    The post-installation script script only altered /etc/nslcd.conf
    if that did exists, which is the wrong behavior.
    (Escaped testing because if you re-install piserver multiple times
    there may still exist a piserver.pem from a previous installation
    that did not use external server)
    
    Ref #72

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -21,12 +21,12 @@ fi
 # Copy LDAP SSL certificate, and enable verification
 #
 
-if [ -d "$DISTROROOT/etc/ssl/certs" ] && [ -f "/etc/ssl/certs/piserver.pem" ]; then
-    cp -a /etc/ssl/certs/piserver.pem "$DISTROROOT/etc/ssl/certs"
-    if [ -f "$DISTROROOT/etc/nslcd.conf" ]; then
-        if [ -n "$EXTERNAL_LDAP_CONFIG" ]; then
-            echo "$EXTERNAL_LDAP_CONFIG" >> "$DISTROROOT/etc/nslcd.conf"
-        else
+if [ -f "$DISTROROOT/etc/nslcd.conf" ]; then
+    if [ -n "$EXTERNAL_LDAP_CONFIG" ]; then
+        echo "$EXTERNAL_LDAP_CONFIG" >> "$DISTROROOT/etc/nslcd.conf"
+    else
+        if [ -d "$DISTROROOT/etc/ssl/certs" ] && [ -f "/etc/ssl/certs/piserver.pem" ]; then
+            cp -a /etc/ssl/certs/piserver.pem "$DISTROROOT/etc/ssl/certs"
             sed -i "s/#ssl off/ssl start_tls/g" "$DISTROROOT/etc/nslcd.conf"
             sed -i "s/#tls_reqcert never/tls_reqcert demand/g" "$DISTROROOT/etc/nslcd.conf"
             sed -i "s#/etc/ssl/certs/ca-certificates.crt#/etc/ssl/certs/piserver.pem#g" "$DISTROROOT/etc/nslcd.conf"
